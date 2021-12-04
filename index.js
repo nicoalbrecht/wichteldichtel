cfc21e2243a51e4ed421e24a7a292a51fc94a5f2
Nico Albrecht <nicolai.albrecht96@gmail.com>
2021-12-04 18:18:42 +0100

    init

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomInt } = require('crypto');
 const nodemailer = require("nodemailer");
+const { MongoClient } = require("mongodb");
+
+const dbClient = new MongoClient('mongodb+srv://wichtel:RRJslsulJa2OBShL@cluster0.8ziar.mongodb.net/wichtel?retryWrites=true&w=majority');
+
 const app = express();
 const port = 3000;
 
@@ -9,7 +13,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const mailTransport = nodemailer.createTransport({sendmail: true});
+// const mailTransport = nodemailer.createTransport({sendmail: true});
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'nicolai.albrecht96@gmail.com',
+        pass: '5sO^jN#JtW!i7'
+    }
+});
 
 const getMapping = (u) => {
     let picked = [];
@@ -19,7 +30,6 @@ const getMapping = (u) => {
         let j = Math.floor(Math.random() * (i + 1));
         [u[i], u[j]] = [u[j], u[i]];
     }
-
     for (let i = 0; i < u.length; i++) {
         let succ = false;
         while (!succ) {
@@ -27,7 +37,8 @@ const getMapping = (u) => {
             if (r !== i && picked.indexOf(u[r]) === -1) {
                 succ = true;
                 picked.push(u[r]);
-                _res.push({from: u[i], to: u[r]});
+                let relIndex = u[i].relations.findIndex(_r => _r.to === u[r].id)
+                _res.push({from: u[i], to: u[r], type: u[i].relations[relIndex].type});
             }
             maxTries--;
             if(maxTries < 1) return false;
@@ -36,22 +47,43 @@ const getMapping = (u) => {
     return _res;
 };
 
+const getPresentIdeas = async(collection, type) => {
+    const presents = await (await collection.find({categories: type})).toArray();
+    for (let i = presents.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [presents[i], presents[j]] = [presents[j], presents[i]];
+    }
+    console.log(presents.slice(0, 5));
+    return presents.slice(0, 5);
+}
+
 app.get('/', (req, res) => {
     res.sendFile('./public/index.html');
 });
 
-app.post('/wichtel', (req, res) => {
-    let u = req.body;
+app.post('/wichtel', async (req, res) => {
+    let u = req.body.users;
+    let enablePresents = req.body.enablePresents;
     let _res = getMapping(u);
     while(_res === false) {
         _res = getMapping(u);
     }
+    await dbClient.connect();
+    const database = dbClient.db('wichtel');
+    const presents = database.collection('presents');
     for(let i = 0; i < _res.length; i++) {
+        let ideas = await getPresentIdeas(presents, _res[i].type);
+        let ideasText = '';
+        if(enablePresents) {
+            for(let j = 0; j < ideas.length; j++) {
+                ideasText += `${ideas[j].title} (${ideas[j].price}€) \n`
+            }
+        }
         mailTransport.sendMail({
-            from: 'no-reply@wichtel-dichtel.de',
+            from: 'nicolai.albrecht96@gmail.com',
             to: _res[i].from.mail,
-            subject: 'Message',
-            text: `Du darfst für ${_res[i].to.name} ein Wichtelgeschenk aussuchen!`
+            subject: 'Wichtel Auslosung',
+            text: `Du darfst für ${_res[i].to.name} ein Wichtelgeschenk aussuchen! \n \n \n Geschenkideen:  \n ${ideasText}`
         }, (err, info) => {
             if(err) console.log(err);
             console.log(info);
--- public/js/main.js
+++ public/js/main.js
@@ -1,84 +1,177 @@
-let userCount = 3;
-
-const fixUserOrder = () => {
-    let usr = document.querySelectorAll('.input-group');
-    for(let i = 0; i < usr.length; i++) {
-        usr[i].querySelector('p').innerHTML = (i+1) + '.';
-        usr[i].querySelector('input[name^=name]').setAttribute('name', `name-${i+1}`);
-        usr[i].querySelector('input[name^=mail]').setAttribute('mail', `mail-${i+1}`);
-        if(i !== 0) usr[i].querySelector('.remove-user-icon').setAttribute('data-id', `${i+1}`);
-        usr[i].setAttribute('data-id', `${i+1}`);
-    }
-}
-
-const handleRemoveEvent = (event) => {
-    userCount--;
-    let t;
-    if(event.target.classList.contains('remove-user-icon')) {
-        t = event.target;
-    } else {
-        t = event.target.closest('.remove-user-icon')
-    }
-    let _id = t.getAttribute('data-id');
-    document.querySelector(`.input-group[data-id="${_id}"]`)?.remove();
-    fixUserOrder();
-}
-
-const addRemoveEventListener = () => {
-    let btns = document.querySelectorAll('.remove-user-icon');
-    for(let i = 0; i < btns.length; i++) {
-        btns[i].onclick = handleRemoveEvent;
-    }
-}
-
-const submitData = () => {
-    let usr = document.querySelectorAll('.input-group');
-    let data = [];
-    for(let i = 0; i < usr.length; i++) {
-        let name = usr[i].querySelector('input[name^=name]').value;
-        let mail = usr[i].querySelector('input[name^=mail]').value;
-        if(name && mail) data.push({id: i, name, mail});
-    }
-    console.log(data);
-    fetch('/wichtel', {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json'
+const app = Vue.createApp({
+    data() {
+        return {
+            categories: ["Frauen","Männer","Partnerin","Partner","Beste Freundin","Bester Freund","Mutter","Vater","Oma","Opa","Teenager","Mädchen","Junge","Kinder","Schwester","Bruder","Schwiegermutter","Eltern","Chef","Mitarbeiter","Kunden"],
+            users: [
+                {id: 0, name: '', mail: '', showRelations: true, relations: [
+                    {to: 1, type: ''},
+                    {to: 2, type: ''},
+                ], specialRelation: ''},
+                {id: 1, name: '', mail: '', showRelations: false, relations: [
+                    {to: 0, type: ''},
+                    {to: 2, type: ''},
+                ], specialRelation: ''},
+                {id: 2, name: '', mail: '', showRelations: false, relations: [
+                    {to: 0, type: ''},
+                    {to: 1, type: ''}
+                    
+                ], specialRelation: ''}
+            ],
+            enablePresents: true,
+            enableComplexRelations: false,
+            defaultRelation: '',
+            formValid: false
+        }
+    },
+    methods: {
+        getUserById(id) {
+            let index = this.users.findIndex(u => u.id === id);
+            if(index !== -1) {
+                return this.users[index];
+            }
         },
-        body: JSON.stringify(data)
-    }).then(res => {
-        return res.json();
-    }).catch(err => {
-        console.log(err);
-    }).then(data => {
-        Swal.fire(
-            'Auiii',
-            'Benachrichtigungen wurde versendet!',
-            'success'
-        );
-        console.log(data);
-    }).catch(err => {
-        console.log(err);
-    });
-}
+        addUser() {
+            let newId = this.users.length;
+            let newRelations = [];
+            for(let i = 0; i < this.users.length; i++) {
+                newRelations.push({from: newId, to: this.users[i].id, type: ''});
+                this.users[i].relations.push({from: this.users[i].id, to: newId, type: ''});
+            }
+            this.users.push({id: newId, name: '', mail: '', relations: newRelations, specialRelation: ''});
+        },
+        removeUser(id) {
+            let index = this.users.findIndex(u => u.id === id);
+            if(index !== -1) {
+                for(let i = 0; i < this.users.length; i++) {
+                    let _i = this.users[i].relations.findIndex(r => r.to === id);
+                    this.users[i].relations.splice(_i, 1);
+                }
+                this.users.splice(index, 1);
+                for(let i = 0; i < this.users.length; i++) {
+                    for(let j = 0; j < this.users.length; j++) {
+                        if(i !== j) {
+                            let _i = this.users[j].relations.findIndex(r => r.to === this.users[i].id);
+                            if(_i !== -1) {
+                                this.users[j].relations[_i].to = i;
+                            }
+                        }
+                    }
+                    this.users[i].id = i;
+                }
+            }
+        },
+        submitData() {
+            let _users = []
+            if(this.enableComplexRelations) {
+                for(let i = 0; i < this.users.length; i++) {
+                    let {id, name, mail} = this.users[i];
+                    let _relations = [];
+                    for(let r = 0; r < this.users[i].relations.length; r++) _relations.push({to: this.users[i].relations[r].to, type: this.users[i].relations[r].type});
+                    _users.push({id, name, mail, relations: _relations});
+                }
+            } else {
+                let _us = [];
+                for(let i = 0; i < this.users.length; i++) {
+                    let {id, name, mail, specialRelation} = this.users[i];
+                    _us.push({id, name, mail, relationToOthers: specialRelation ? specialRelation : this.defaultRelation});
+                    _users.push({id, name, mail, relations: []});
+                }
+                for(let i = 0; i < _us.length; i++) {
+                    let {id, relationToOthers} = _us[i];
+                    for(let j = 0; j < _users.length; j++) {
+                        if(id !== _users[j].id) {
+                            _users[j].relations.push({to: id, type: relationToOthers});
+                        }
+                    }
+                }
+            }
 
-(() => {
-    document.querySelector('#add-user-btn').addEventListener('click', () => {
-        userCount++;
-        let html = `
-            <div class="input-group">
-                <p>${userCount}.</p>
-                <input type="text" name="name-${userCount}" id="name-${userCount}" placeholder="Name">
-                <input type="text" name="mail-${userCount}" id="mail-${userCount}" placeholder="E-Mail">
-                <i class="fas fa-trash remove-user-icon fa-2x" data-id="${userCount}"></i>
-            </div>
-        `;
-        document.querySelector('#form').insertAdjacentHTML('beforeend', html);
-        addRemoveEventListener();
-    });
+            for(let i = 0; i < _users.length; i++) {
+                for(let r = 0; r < _users[i].relations.length; r++) {
+                    if(_users[i].relations[r].type === '') {
+                        _users[i].relations[r].type = this.defaultRelation;
+                    }
+                }
+            }
+            fetch('/wichtel', {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({users, enablePresents: this.enablePresents})
+            }).then(res => {
+                return res.json();
+            }).catch(err => {
+                console.log(err);
+            }).then(data => {
+                Swal.fire(
+                    'Auiii',
+                    'Benachrichtigungen wurde versendet!',
+                    'success'
+                );
+                console.log(data);
+            }).catch(err => {
+                console.log(err);
+            });
+        },
+        toggleRelations(id) {
+            let index = this.users.findIndex(u => u.id === id);
+            if(index !== -1) {
+                this.users[index].showRelations = !this.users[index].showRelations;
+            }
+        },
+        showSettings() {
+            Swal.fire({
+                template: '#settings-modal',
+                width: '50%',
+                willOpen: (e) => {
+                    e.querySelector('#enable-presents').checked = this.enablePresents;
+                    e.querySelector('#enable-detailed-relations').checked = this.enableComplexRelations;
+                },
+                preConfirm: () => {
+                    return {
+                        enablePresents: document.querySelector('#enable-presents').checked,
+                        complexRelations: document.querySelector('#enable-detailed-relations').checked
+                    }
+                  }
+            }).then((result) => {
+                if(result.isConfirmed) {
+                    this.enablePresents = result.value.enablePresents;
+                    this.enableComplexRelations = result.value.complexRelations;
+                }
+            });
+        }
+    },
+    computed: {
+        validateName() {
+            return id => {
+                let index = this.users.findIndex(u => u.id === id);
+                if(index !== -1) {
+                    return this.users[index].name.length > 1;
+                }
+                return false;
+            }
+        },
+        validateMail() {
+            return id => {
+                let index = this.users.findIndex(u => u.id === id);
+                if(index !== -1) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(this.users[index].mail);
+                }
+                return false;
+            }
+        },
+        validateForm() {
+            let valid = true;
+            for(let i = 0; i < this.users.length; i++) {
+                valid = valid && (this.users[i].name.length > 1);
+                valid = valid && (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(this.users[i].mail));
+            }
+            return valid;
+        }
+    }
+});
 
-    document.querySelector('#wichtel-btn').addEventListener('click', submitData);
-    addRemoveEventListener();
-})();
+app.mount('#content');
